52645448d8ffa190243d5412f9745f168a90c1c8
Harry Chuang <harry.chuang@goshopmatic.com>
2025-09-11 20:47:00 +0800

    chore(cms): temporarily allow CORS from all origins for local debugging

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -5,12 +5,9 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173'
-      ],
+      origin: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: '*',
       keepHeaderOnError: true
     }
   },
