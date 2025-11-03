f7c09c503ba0bf4624deae978f0c6f8e4c7474e9
Harry Chuang <harry.chuang@goshopmatic.com>
2025-11-04 01:24:55 +0800

    Updated sounds

--- portfolio/src/components/ScrollIndicator.tsx
+++ portfolio/src/components/ScrollIndicator.tsx
@@ -137,7 +137,7 @@ const ScrollIndicator: React.FC<ScrollIndicatorProps> = ({
       // Like/Unlike 按鈕模式
       soundFile = isLiked 
         ? '/assets/sound/8-Bit Powerup Sound Effect.mp3'  // 已 liked，點擊後 unlike
-        : '/assets/sound/8-Bit Coin Pickup Sound.mp3';                       // 未 liked，點擊後 like
+        : '/assets/sound/Win Sound Effect 1.mp3';    // 未 liked，點擊後 like
     } else {
       // Go to top 按鈕模式
       soundFile = '/assets/sound/8-Bit Powerup Sound Effect.mp3';
