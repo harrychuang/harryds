608f3dfef79f73493e7b274fcc0ddda62477d4c7
Harry Chuang <harry.chuang@goshopmatic.com>
2025-11-08 20:21:30 +0800

    Added new contents

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -10,6 +10,15 @@ import award01 from '../../assets/imgs/awards/award-01.png';
 import award02 from '../../assets/imgs/awards/award-02.png';
 import award03 from '../../assets/imgs/awards/award-03.png';
 import award04 from '../../assets/imgs/awards/award-04.png';
+import logoAwwrated from '../../assets/imgs/logos/logo-awwrated-dark.png';
+import logoKkday from '../../assets/imgs/logos/logo-kkday-dark.png';
+import logoNownews from '../../assets/imgs/logos/logo-nownews-dark.png';
+import logoWalkerland from '../../assets/imgs/logos/logo-walkerland-dark.png';
+import logoIxda from '../../assets/imgs/logos/logo-ixda-dark.png';
+import logoAapd from '../../assets/imgs/logos/logo-aapd-dark.png';
+import logoQnap from '../../assets/imgs/logos/logo-qnap-dark.png';
+import logoUxy from '../../assets/imgs/logos/logo-uxy-dark.png';
+import logoShopmatic from '../../assets/imgs/logos/logo-shopmatic-dark.png';
 import '../pages/Home.scss';
 
 const About: React.FC = () => {
@@ -322,6 +331,53 @@ const About: React.FC = () => {
                 </li>
               </ul>
             </div>
+          </div>
+        </section>
+
+        <section className="home__clients" aria-labelledby="about-clients-title">
+          <div className="home__intro-grid">
+            <div className="home__intro-column home__intro-column--secondary" aria-hidden="true" />
+            <div className="home__intro-column home__intro-column--primary">
+              <h2 id="about-clients-title" className="home__intro-title feed-detail-overlay__section-title">
+                Clients &amp; Partners<span className="feed-detail-overlay__cursor">_</span>
+              </h2>
+              <div className="home__intro-body">
+                <p>
+                  Serving startups and enterprises alike:<br />
+                  Product Design / Web Design /<br />
+                  Design System Training &amp; Consulting.
+                </p>
+                <p>
+                  A Systems-driven approach that helps teams ship faster and improve consistency.
+                </p>
+              </div>
+              <div className="home__clients-logos">
+                <img src={logoAwwrated} alt="awwrated logo" className="home__clients-logo home__clients-logo--awwrated" />
+                <img src={logoKkday} alt="kkday logo" className="home__clients-logo home__clients-logo--kkday" />
+                <img src={logoNownews} alt="NOWnews logo" className="home__clients-logo home__clients-logo--nownews" />
+                <img src={logoWalkerland} alt="Walkerland logo" className="home__clients-logo home__clients-logo--walkerland" />
+                <img src={logoIxda} alt="IXDA logo" className="home__clients-logo home__clients-logo--ixda" />
+                <img src={logoAapd} alt="AAPD logo" className="home__clients-logo home__clients-logo--aapd" />
+                <img src={logoQnap} alt="QNAP logo" className="home__clients-logo home__clients-logo--qnap" />
+                <img src={logoUxy} alt="UXY logo" className="home__clients-logo home__clients-logo--uxy" />
+                <img src={logoShopmatic} alt="Shopmatic logo" className="home__clients-logo home__clients-logo--shopmatic" />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="home__background" aria-labelledby="about-background-title">
+          <div className="home__background-inner">
+            <h2 id="about-background-title" className="home__background-title feed-detail-overlay__section-title">
+              My design background<span className="feed-detail-overlay__cursor">_</span>
+            </h2>
+            <p className="home__background-description">
+              My design inspiration didn’t come from textbooks,<br />
+              but from the startup sound of the Famicom (NES).<br />
+              Japanese culture of the 1980s, 8-bit pixels, tokusatsu, and anime taught me to tell stories with images. I once aimed to become a manga artist or game illustrator~<br />
+              Now I turn that obsession into a design methodology,<br />
+              building products that are more loved and more usable.
+            </p>
           </div>
         </section>
       </main>
