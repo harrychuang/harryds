28d07ef1a5b45b05ea124ef91a8ae5df21873245
Harry Chuang <harry.chuang@goshopmatic.com>
2025-12-06 00:26:58 +0800

    Updated

--- portfolio/src/pages/About.tsx
+++ portfolio/src/pages/About.tsx
@@ -431,7 +431,7 @@ const About: React.FC = () => {
     const initialTop = introSectionRef.current.offsetTop;
     gsap.set(introVisualRef.current, { 
       top: initialTop,
-      opacity: 1  // 確保可見
+      opacity: 1
     });
   }, []); // 空依賴陣列，只在首次掛載時執行
 
@@ -483,7 +483,7 @@ const About: React.FC = () => {
     const rotationElement = visualElement.querySelector('.home__intro-rotation') as HTMLElement;
     
     gsap.to(visualElement, {
-      x: '-48vw',
+      x: window.innerWidth <= 1200 ? '-48vw' : window.innerWidth <= 1400 ? '-50vw' : '-48vw',
       y: '-=0',  // 向上移動 200px
       scrollTrigger: {
         trigger: introSectionRef.current,
@@ -501,7 +501,7 @@ const About: React.FC = () => {
     // 同時改變 HarryAnimation 的寬度
     if (rotationElement) {
       gsap.to(rotationElement, {
-        width: '1900px',
+        width: window.innerWidth <= 1200 ? '1400px' : window.innerWidth <= 1400 ? '1700px' : '1900px',
         scrollTrigger: {
           trigger: introSectionRef.current,
           start: 'bottom 50%',
@@ -1053,7 +1053,7 @@ const About: React.FC = () => {
 
         <div className="home__intro-visual" aria-hidden="true" ref={introVisualRef}>
           <HarryAnimation
-            width={'2000px'}
+            width={window.innerWidth <= 1200 ? '1500px' : window.innerWidth <= 1400 ? '1800px' : '2000px'}
             autoPlay={false}
             className="home__intro-rotation"
             frame={rotationFrame}
@@ -1361,7 +1361,7 @@ const About: React.FC = () => {
                   </a>
                 </span>
               </div>
-              <div className="home__contact-animation" ref={contactAnimationRef} style={{ position: 'absolute', bottom: -100, right: -200 }}>
+              <div className="home__contact-animation" ref={contactAnimationRef}>
                 <HarryAnimation
                   type="usemac"
                   frameDuration={750}
